7f4baf63fd2398865c1596b4b446e8fc5910ff78
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-01 17:41:51 -0300

    Exercise complete

---
+++ AutomateContractProcessing/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace AutomateContractProcessing.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
